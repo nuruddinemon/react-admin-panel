c7cc899b12d80ed3923a1e155ab102134b10c6e9
Nur Uddin Emon <lifebd12345@gmail.com>
2023-11-03 22:29:53 +0600

    add: new files

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/react-admin-panel/",
   plugins: [react()],
-  base: "/react-admin-panel",
 });
